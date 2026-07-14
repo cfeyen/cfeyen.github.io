9539682cf081b0a5f4ff343835be9dfd997e7477
cfeyen <cafeyen21@gmail.com>
2026-07-14 18:56:57 -0500

    Add Montana Solitaire

---
+++ pkg/montana_solitaire_bg.wasm.d.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue__core_7d5f0a2ba6a62c33___result__Result_____wasm_bindgen_c3a489c3fd0001b2___JsError___true_: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___js_sys_d753d544966fe7e7___Array__web_sys_f4b8949b1211710f___features__gen_ResizeObserver__ResizeObserver______true_: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue______true__1_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue______true__4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue______true__5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue______true__6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue______true__7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue______true__8: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue______true__9: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke___wasm_bindgen_c3a489c3fd0001b2___JsValue______true__10: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_c3a489c3fd0001b2___convert__closures_____invoke_______true_: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
