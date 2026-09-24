7916a7d58003f04e176de5078c95586c7c2298fa
cfeyen <cafeyen21@gmail.com>
2026-09-23 22:53:40 -0500

    Update CSCSCA interface to be good

--- cscsca/script.js
+++
@@ -1,6 +0,0 @@
-function log_phones(msg, phones) {
-    let logs = document.getElementById("cscsca-logs");
-    let log = document.createElement("p");
-    log.innerText = `${msg}:\n${phones}`;
-    logs.appendChild(log);
-}
